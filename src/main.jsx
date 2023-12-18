c46946852de6c6949399f137c5bf819784ab95c1
임민영 <raccoon@shinsegae.com>
2023-12-19 02:23:44 +0900

    ddat amplify + cognito

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,43 @@ import App from './App.jsx'
 import './index.css'
 import {Amplify} from 'aws-amplify';
 import amplifyconfig from './amplifyconfiguration.json';
-import {Authenticator} from "@aws-amplify/ui-react";
+import {Authenticator, useTheme, View} from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {Image} from '@aws-amplify/ui-react';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 
 Amplify.configure(amplifyconfig);
 
+const queryClient = new QueryClient();
+
+const components = {
+    Header() {
+        const {tokens} = useTheme();
+        return (
+            <View textAlign="center" padding={tokens.space.large}>
+                <Image
+                    height="35%"
+                    width="35%"
+                    alt="DDAT logo"
+                    src="public/ddat.png"
+                />
+            </View>
+        );
+    }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Authenticator>
-            {({signOut, user}) => (
-                <main>
-                    <h1>Hello {user.username}</h1>
-                    <button onClick={signOut}>Sign out</button>
-                    <App/>
-                </main>
+        <Authenticator components={components}>
+            {({signOut}) => (
+                <QueryClientProvider client={queryClient}>
+                    <ReactQueryDevtools initialIsOpen={true}/>
+                    <main>
+                        <App/>
+                        <button  onClick={signOut}>Sign out</button>
+                    </main>
+                </QueryClientProvider>
             )}
         </Authenticator>
     </React.StrictMode>,
